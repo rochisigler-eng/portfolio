55fa6e1bdb4be4c5ab47b6815193bee8a38b2a2a
rochi <rochi.sigler@gmail.com>
2026-02-17 15:37:31 -0300

    modified data.js

--- js/data.js
+++ js/data.js
@@ -1,87 +1,7 @@
 export const workPortfolio = [
     {
         id: 0,
-        type: "React",
-        name: "One Time Password Generator App",
-        img: "assets/projects/react/password-generator.png",
-        link: "https://one-password-generator-rs.netlify.app/",
-        status: "Completed",
-        description: "A React app that generates secure one-time passwords (OTPs) with a single click.",
-        date: "01/02/2026"
-    },
-    {
-        id: 1,
-        type: "React",
-        name: "Shopping List Search App",
-        img: "assets/projects/react/shopping-list-search.png",
-        link: "https://shopping-list-search-rs.netlify.app/",
-        status: "Completed",
-        description: "A React app that lets users search and filter items in a shopping list with instant results.",
-        date: "31/01/2026"
-    },
-    {
-        id: 2,
-        type: "React",
-        name: "Superhero Form App",
-        img: "assets/projects/react/superhero.png",
-        link: "https://superhero-form-rs.netlify.app/",
-        status: "Completed",
-        description: "A React form app to create and manage superhero profiles using controlled inputs and state.",
-        date: "30/01/2026"
-    },
-    {
-        id: 3,
-        type: "React",
-        name: "Fruit Search App",
-        img: "assets/projects/react/fuit-search.png",
-        link: "https://fruit-search-app-rs.netlify.app/",
-        status: "Completed",
-        description: "A React app that lets users search for fruits in real time and displays matching results dynamically",
-        date: "29/01/2026"
-    },
-    {
-        id: 4,
-        type: "React",
-        name: "Event RSVP Form App",
-        img: "assets/projects/react/event-rsvp.png",
-        link: "https://event-rsvp-rs.netlify.app/",
-        status: "Completed",
-        description: "A React app that lets users submit their attendance details for an event and manages form state and validation.",
-        date: "28/01/2026"
-    },
-    {
-        id: 5,
-        type: "React",
-        name: "Currency Converter App",
-        img: "assets/projects/react/currency-converter.png",
-        link: "https://currency-converter-app-rs.netlify.app/",
-        status: "Completed",
-        description: "A React app that converts amounts between different currencies using real-time exchange rates.",
-        date: "27/01/2026"
-    },
-    {
-        id: 6,
-        type: "Javascript",
-        name: "Shopping Cart App",
-        img: "assets/projects/javascript/shopping-cart.png",
-        link: "https://shopping-cart-javascript-rs.netlify.app/",
-        status: "Completed",
-        description: "A JavaScript shopping cart app that lets users add, remove, and update items while automatically calculating the total.",
-        date: "26/01/2026"
-    },
-    {
-        id: 7,
-        type: "Javascript",
-        name: "Recipe Ingredient Converter App",
-        img: "assets/projects/javascript/recipe-converter.png",
-        link: "https://recipe-ingredient-converter-rs.netlify.app/",
-        status: "Completed",
-        description: "A simple JavaScript app that converts recipe ingredient quantities between different units for easy cooking adjustments.",
-        date: "25/01/2026"
-    },
-    {
-        id: 8,
-        type: "Javascript",
+        type: "JAVASCRIPT",
         name: "Music Player App",
         img: "assets/projects/javascript/music-player.png",
         link: "https://music-player-rs.netlify.app/",
@@ -90,58 +10,18 @@ export const workPortfolio = [
         date: "24/01/2026"
     },
     {
-        id: 9,
-        type: "Javascript",
-        name: "Todo App",
-        img: "assets/projects/javascript/todo.png",
-        link: "https://todo-app-rs.netlify.app/",
+        id: 1,
+        type: "JAVASCRIPT",
+        name: "Shopping Cart App",
+        img: "assets/projects/javascript/shopping-cart.png",
+        link: "https://shopping-cart-javascript-rs.netlify.app/",
         status: "Completed",
-        description: "A JavaScript to-do app for adding, tracking, and completing tasks.",
-        date: "23/01/2026"
+        description: "A JavaScript shopping cart app that lets users add, remove, and update items while automatically calculating the total.",
+        date: "26/01/2026"
     },
-    {
-        id: 10,
-        type: "Javascript",
-        name: "Drum Machine App",
-        img: "assets/projects/javascript/drum-machine.png",
-        link: "https://drum-machine-rs.netlify.app/",
-        status: "Completed",
-        description: "A JavaScript drum machine that plays sounds via interactive pads.",
-        date: "22/01/2026"
-    },
-    {
-        id: 11,
-        type: "Javascript",
-        name: "Forum LeaderBoard App",
-        img: "assets/projects/javascript/forum-leaderboard.png",
-        link: "https://forum-leaderboard-rs.netlify.app/",
-        status: "Completed",
-        description: "A JavaScript leaderboard that ranks forum users by activity.",
-        date: "21/01/2026"
-    },
-    {
-        id: 12,
-        type: "Javascript",
-        name: "fCC Authors Page",
-        img: "assets/projects/javascript/authors-page.png",
-        link: "https://fcc-authors-page-rs.netlify.app/",
-        status: "Completed",
-        description: "A JavaScript page that displays author information from freeCodeCamp.",
-        date: "20/01/2026"
-    },
-    {
-        id: 13,
-        type: "Javascript",
-        name: "Complaint Form App",
-        img: "assets/projects/javascript/complaint-form.png",
-        link: "https://form-validation-javascript-rs.netlify.app/",
-        status: "Completed",
-        description: "A JavaScript complaint form that collects and submits user feedback.",
-        date: "31/12/2025"
-    },
-    {
-        id: 14,
-        type: "Javascript",
+     {
+        id: 2,
+        type: "JAVASCRIPT",
         name: "Decimal to Binary App",
         img: "assets/projects/javascript/decimal-binary.png",
         link: "https://decimal-binary-converter-rs.netlify.app/",
@@ -150,28 +30,8 @@ export const workPortfolio = [
         date: "8/1/2025"
     },
     {
-        id: 15,
-        type: "Javascript",
-        name: "BookMark Manager App",
-        img: "assets/projects/javascript/bookmark-manager-app.png",
-        link: "bookmark-manager-app-rs.netlify.app",
-        status: "Completed",
-        description: "A JavaScript bookmark manager that lets users categorize, add, view, and delete bookmarks.",
-        date: "5/1/2025"
-    },
-    {
-        id: 16,
-        type: "Javascript",
-        name: "Sign Up Landing",
-        img: "assets/projects/javascript/form_validation.png",
-        link: "https://formulario-validado-javascript-rs.netlify.app/",
-        status: "Completed",
-        description: "A JavaScript project that validates form inputs in real time, ensuring correct data entry and providing user feedback before submission.",
-        date: "5/20/2025"
-    },
-    {
-        id: 17,
-        type: "Javascript",
+        id: 3,
+        type: "JAVASCRIPT",
         name: "Pokedex",
         img: "assets/projects/javascript/pokedex.png",
         link: "https://pokedex-project-rs.netlify.app/",
@@ -179,19 +39,9 @@ export const workPortfolio = [
         description: "A JavaScript project that uses the Pokémon API to display detailed Pokédex information, including search functionality, images, and basic data for each Pokémon.",
         date: "5/20/2025"
     },
-    {
-        id: 18,
-        type: "Javascript",
-        name: "Social Media",
-        img: "assets/projects/javascript/red_social.png",
-        link: "https://red-social-rs.netlify.app/",
-        status: "Completed",
-        description: "A social media project built with JavaScript that allows users to create posts, interact with content, and simulate core features of a social platform, with a focus on user experience and state management.",
-        date: "5/25/2025"
-    },
-    {
-        id: 19,
-        type: "Javascript",
+     {
+        id: 4,
+        type: "JAVASCRIPT",
         name: "Calculator App",
         img: "assets/projects/javascript/calculadora.png",
         link: "https://calculator-project-rs.netlify.app/",
@@ -200,8 +50,158 @@ export const workPortfolio = [
         date: "5/25/2025"
     },
     {
+        id: 5,
+        type: "REACT",
+        name: "One Time Password Generator App",
+        img: "assets/projects/react/password-generator.png",
+        link: "https://one-password-generator-rs.netlify.app/",
+        status: "Completed",
+        description: "A React app that generates secure one-time passwords (OTPs) with a single click.",
+        date: "01/02/2026"
+    },
+    {
+        id: 6,
+        type: "REACT",
+        name: "Shopping List Search App",
+        img: "assets/projects/react/shopping-list-search.png",
+        link: "https://shopping-list-search-rs.netlify.app/",
+        status: "Completed",
+        description: "A React app that lets users search and filter items in a shopping list with instant results.",
+        date: "31/01/2026"
+    },
+    {
+        id: 7,
+        type: "REACT",
+        name: "Superhero Form App",
+        img: "assets/projects/react/superhero.png",
+        link: "https://superhero-form-rs.netlify.app/",
+        status: "Completed",
+        description: "A React form app to create and manage superhero profiles using controlled inputs and state.",
+        date: "30/01/2026"
+    },
+    {
+        id: 8,
+        type: "REACT",
+        name: "Fruit Search App",
+        img: "assets/projects/react/fuit-search.png",
+        link: "https://fruit-search-app-rs.netlify.app/",
+        status: "Completed",
+        description: "A React app that lets users search for fruits in real time and displays matching results dynamically",
+        date: "29/01/2026"
+    },
+    {
+        id: 9,
+        type: "REACT",
+        name: "Event RSVP Form App",
+        img: "assets/projects/react/event-rsvp.png",
+        link: "https://event-rsvp-rs.netlify.app/",
+        status: "Completed",
+        description: "A React app that lets users submit their attendance details for an event and manages form state and validation.",
+        date: "28/01/2026"
+    },
+    {
+        id: 10,
+        type: "REACT",
+        name: "Currency Converter App",
+        img: "assets/projects/react/currency-converter.png",
+        link: "https://currency-converter-app-rs.netlify.app/",
+        status: "Completed",
+        description: "A React app that converts amounts between different currencies using real-time exchange rates.",
+        date: "27/01/2026"
+    },
+    {
+        id: 11,
+        type: "JAVASCRIPT",
+        name: "Recipe Ingredient Converter App",
+        img: "assets/projects/javascript/recipe-converter.png",
+        link: "https://recipe-ingredient-converter-rs.netlify.app/",
+        status: "Completed",
+        description: "A simple JavaScript app that converts recipe ingredient quantities between different units for easy cooking adjustments.",
+        date: "25/01/2026"
+    },
+    {
+        id: 12,
+        type: "JAVASCRIPT",
+        name: "Todo App",
+        img: "assets/projects/javascript/todo.png",
+        link: "https://todo-app-rs.netlify.app/",
+        status: "Completed",
+        description: "A JavaScript to-do app for adding, tracking, and completing tasks.",
+        date: "23/01/2026"
+    },
+    {
+        id: 13,
+        type: "JAVASCRIPT",
+        name: "Drum Machine App",
+        img: "assets/projects/javascript/drum-machine.png",
+        link: "https://drum-machine-rs.netlify.app/",
+        status: "Completed",
+        description: "A JavaScript drum machine that plays sounds via interactive pads.",
+        date: "22/01/2026"
+    },
+    {
+        id: 14,
+        type: "JAVASCRIPT",
+        name: "Forum LeaderBoard App",
+        img: "assets/projects/javascript/forum-leaderboard.png",
+        link: "https://forum-leaderboard-rs.netlify.app/",
+        status: "Completed",
+        description: "A JavaScript leaderboard that ranks forum users by activity.",
+        date: "21/01/2026"
+    },
+    {
+        id: 15,
+        type: "JAVASCRIPT",
+        name: "fCC Authors Page",
+        img: "assets/projects/javascript/authors-page.png",
+        link: "https://fcc-authors-page-rs.netlify.app/",
+        status: "Completed",
+        description: "A JavaScript page that displays author information from freeCodeCamp.",
+        date: "20/01/2026"
+    },
+    {
+        id: 16,
+        type: "JAVASCRIPT",
+        name: "Complaint Form App",
+        img: "assets/projects/javascript/complaint-form.png",
+        link: "https://form-validation-javascript-rs.netlify.app/",
+        status: "Completed",
+        description: "A JavaScript complaint form that collects and submits user feedback.",
+        date: "31/12/2025"
+    },
+    {
+        id: 17,
+        type: "JAVASCRIPT",
+        name: "BookMark Manager App",
+        img: "assets/projects/javascript/bookmark-manager-app.png",
+        link: "bookmark-manager-app-rs.netlify.app",
+        status: "Completed",
+        description: "A JavaScript bookmark manager that lets users categorize, add, view, and delete bookmarks.",
+        date: "5/1/2025"
+    },
+    {
+        id: 18,
+        type: "JAVASCRIPT",
+        name: "Sign Up Landing",
+        img: "assets/projects/javascript/form_validation.png",
+        link: "https://formulario-validado-javascript-rs.netlify.app/",
+        status: "Completed",
+        description: "A JavaScript project that validates form inputs in real time, ensuring correct data entry and providing user feedback before submission.",
+        date: "5/20/2025"
+    },
+    {
+        id: 19,
+        type: "JAVASCRIPT",
+        name: "Social Media",
+        img: "assets/projects/javascript/red_social.png",
+        link: "https://red-social-rs.netlify.app/",
+        status: "Completed",
+        description: "A social media project built with JavaScript that allows users to create posts, interact with content, and simulate core features of a social platform, with a focus on user experience and state management.",
+        date: "5/25/2025"
+    },
+    {
         id: 20,
-        type: "Javascript",
+        type: "JAVASCRIPT",
         name: "Login",
         img: "assets/projects/javascript/login.png",
         link: "https://login-validado-con-javascript-rs.netlify.app/",
@@ -241,106 +241,12 @@ export const workPortfolio = [
     },
     {
         id: 24,
-        type: "Javascript",
+        type: "JAVASCRIPT",
         name: "Markdown to HTML converter",
         img: "assets/projects/javascript/markdown.png",
         link: "https://html-markdown-rs.netlify.app/",
         status: "Completed",
         description: "A JavaScript project that converts Markdown text into HTML, allowing real-time preview and making it easier to create formatted content.",
-        date: "5/25/2025"
-    },
-]
-
-
-const workPortfolioEsp = [
-    {
-        id: 0,
-        type: "Javascript",
-        name: "Sign Up Landing",
-        img: "assets/projects/javascript/form_validation.png",
-        link: "https://formulario-validado-javascript-rs.netlify.app/",
-        status: "Completed",
-        description: "Projecto que valida inputs en tiempo real, validando que la información sea correcta.",
-        date: "5/20/2025"
-    },
-    {
-        id: 1,
-        type: "Javascript",
-        name: "Pokedex",
-        img: "assets/projects/javascript/pokedex.png",
-        link: "https://pokedex-project-rs.netlify.app/",
-        status: "Completed",
-        description: "Proyecto de JavaScript que usa la API de Pokémon para mostrar información detallada de una Pokédex, incluyendo búsqueda, imágenes y datos básicos de cada Pokémon.",
-        date: "5/25/2025"
-    },
-    {
-        id: 2,
-        type: "Javascript",
-        name: "Social Media",
-        img: "assets/projects/javascript/red_social.png",
-        link: "https://red-social-rs.netlify.app/",
-        status: "Completed",
-        description: "Proyecto de red social desarrollado en JavaScript que permite crear publicaciones, interactuar con contenido y simular funcionalidades básicas de una plataforma social, con enfoque en la experiencia de usuario y el manejo del estado.",
-        date: "5/22/2025"
-    },
-    {
-        id: 3,
-        type: "Javascript",
-        name: "Calculator App",
-        img: "assets/projects/javascript/calculadora.png",
-        link: "https://calculator-project-rs.netlify.app/",
-        status: "Completed",
-        description: "Creando mi propia calculadora",
-        date: "5/25/2025"
-    },
-    {
-        id: 4,
-        type: "Javascript",
-        name: "Login",
-        img: "assets/projects/javascript/login.png",
-        link: "https://login-validado-con-javascript-rs.netlify.app/",
-        status: "Completed",
-        description: "Probando distintas cosas con el input.",
-        date: "5/25/2025"
-    },
-    {
-        id: 5,
-        type: "HTML & CSS",
-        name: "Social Media Dashboard",
-        img: "assets/projects/html_css/social_media.png",
-        link: "https://social-media-rs.netlify.app/",
-        status: "Completed",
-        description: "Creación de landing pages. Práctica de HTML y CSS. Cambio de estilos de plantilla. Adaptación a dispositivos móviles. Algunos de mis primeros trabajos.",
-        date: "5/25/2025"
-    },
-    {
-        id: 6,
-        type: "HTML & CSS",
-        name: "Captivate Landing",
-        img: "assets/projects/html_css/captivate.png",
-        link: "https://captivate-rs.netlify.app/",
-        status: "Completed",
-        description: " Creación de landing pages. Práctica de HTML y CSS.",
-        date: "5/25/2025"
-    },
-    {
-        id: 7,
-        type: "HTML & CSS",
-        name: "City Skyline",
-        img: "assets/projects/html_css/city_skyline.png",
-        link: "https://city-skyline-rs.netlify.app/",
-        status: "Completed",
-        description: "Práctica de HTML y CSS.",
-        date: "5/25/2025"
-    },
-    {
-        id: 8,
-        type: "Javascript",
-        name: "Markdown to HTML converter",
-        img: "assets/projects/javascript/markdown.png",
-        link: "https://html-markdown-rs.netlify.app/",
-        status: "Completed",
-        description: "Proyecto en JavaScript que convierte texto Markdown en HTML, permitiendo previsualizar contenido en tiempo real y facilitando la creación de textos formateados.",
         date: "5/25/2025"
     },
 ]
